f0488b78dd8d32d3c4511d5b9a4770333be5882d
cra88y <cam@loudstyle.com>
2025-10-21 02:49:03 +0000

    fix bundler issue

--- ts/src/main.ts
+++ ts/src/main.ts
@@ -1,7 +1,6 @@
 function InitModule(ctx: nkruntime.Context, logger: nkruntime.Logger, nk: nkruntime.Nakama, initializer: nkruntime.Initializer) {
-  logger.info('TypeScript module loaded.')
+  logger.info('TypeScript module loaded.');
 }
 
-this.InitModule = InitModule
-
-
+// Reference InitModule to avoid it getting removed on build
+!InitModule && InitModule.bind(null);
