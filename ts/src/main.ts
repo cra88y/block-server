b316d5250dc0204fb105666913ac06f29df56548
Cameron (Hire Me) <cam@loudstyle.com>
2025-10-20 20:52:16 -0500

    Update main.ts

--- ts/src/main.ts
+++ ts/src/main.ts
@@ -2,6 +2,3 @@ let InitModule: nkruntime.InitModule =
         function(ctx: nkruntime.Context, logger: nkruntime.Logger, nk: nkruntime.Nakama, initializer: nkruntime.Initializer) {
     logger.info("TypeScript module loaded.");
 }
-
-// Reference InitModule to avoid it getting removed on build
-!InitModule && InitModule.bind(null);
