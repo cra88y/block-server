8fddcc068015d08d69bb8d0ba34d546d85bfb132
Cameron (Hire Me) <cam@loudstyle.com>
2025-10-20 20:52:55 -0500

    Update main.ts

--- ts/src/main.ts
+++ ts/src/main.ts
@@ -2,3 +2,5 @@ let InitModule: nkruntime.InitModule =
         function(ctx: nkruntime.Context, logger: nkruntime.Logger, nk: nkruntime.Nakama, initializer: nkruntime.Initializer) {
     logger.info("TypeScript module loaded.");
 }
+
+export default InitModule;
