851b7908f7f5a919fb4b52ab36ffce97c31cedfc
Cameron (Hire Me) <cam@loudstyle.com>
2025-10-20 20:57:14 -0500

    Update main.ts

--- ts/src/main.ts
+++ ts/src/main.ts
@@ -1,4 +1,3 @@
-import { InitModule } from "@heroiclabs/nakama-runtime";
 let NkInitModule: nkruntime.InitModule =
         function(ctx: nkruntime.Context, logger: nkruntime.Logger, nk: nkruntime.Nakama, initializer: nkruntime.Initializer) {
     logger.info("TypeScript module loaded.");
