e59e13c7265b011ffeb435f06d0c4f4c44fd8c9d
cra88y/pc <cam@loudstyle.com>
2026-05-05 17:29:42 -0500

    fix piece style payload

--- go/items/lootbox.go
+++ go/items/lootbox.go
@@ -319,7 +319,7 @@ func pickRandomItemFromPool(poolName string, ownedItems map[string][]uint32) (st
 	}
 
 	picked := available[rand.Intn(len(available))]
-	return typeToStorageKey(picked.Type), picked.ID
+	return picked.Type, picked.ID
 }
 
 func randomRange(min, max int) int {
